a9147482bfd5daee42ed175aba1a45363da79594
rob2universe <robert.emsbach@gmail.com>
2021-01-11 19:16:32 +0800

    clean

--- CreateContainerInstace.ps1
+++
@@ -1,9 +0,0 @@
-$rgName = Read-Host -Prompt 'Which resource group name should be used?'
-$acrName = Read-Host -Prompt 'What is the registry name?'
-$containerName = Read-Host -Prompt 'Which name should be used for the container?'
-$dnsName = Read-Host -Prompt 'Which DNS name should be used for the unique container instance URL?'
-
-$creds = Get-AzContainerRegistryCredential -ResourceGroup $rgName -Name $acrName
-$regCred = New-Object System.Management.Automation.PSCredential ($creds.Username, (ConvertTo-SecureString $creds.Password -AsPlainText -Force))
-$image = $acrName +'.azurecr.io/bpmrun-add-to-dockerimg:1.0'
-New-AzContainerGroup -ResourceGroupName $rgName -Name $containerName -Image $image -DnsNameLabel $dnsName -RegistryCredential $regCred -OsType Linux -IpAddressType Public -Port @(8080) -Cpu 1 -MemoryInGB 0.5
